e99a10f00540bbe48da1cedafb2888ad41590377
Umesh02 <u.teach.and.i.learn@gmail.com>
2020-06-21 13:13:43 +0530

    implemented Author routes and Search functionality

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const authorRouter = require('./routes/authors');
@@ -19,7 +20,7 @@ app.set('layout', 'layouts/layout');
 
 app.use(expressLayouts);
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 const mongoose = require('mongoose');
 // mongoose.connect('process.env.DATABASE_URL, {
 //   useNewUrlParser: true,
